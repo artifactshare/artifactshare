a83044a131cf657797972468afb9d218ac83af53
coji <coji@techtalk.jp>
2026-09-11 10:13:33 +0900

    fix: restore viewer route component props

--- apps/web/app/routes/a.$id/index.tsx
+++ apps/web/app/routes/a.$id/index.tsx
@@ -1,8 +1,8 @@
+import ViewerRoute, { ErrorBoundary as ViewerErrorBoundary } from './+viewer'
+
 export { loader } from './+loader.server'
-export {
-  default,
-  meta,
-  ErrorBoundary,
-  AgentHelpContent,
-  buildPreauthCliOpenCommand,
-} from './+viewer'
+export { meta, AgentHelpContent, buildPreauthCliOpenCommand } from './+viewer'
+
+// Direct component exports let React Router inject loader and error props.
+export default ViewerRoute
+export const ErrorBoundary = ViewerErrorBoundary
